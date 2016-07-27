d40773761e83d2a4f2e95bb38246376d4d81ff73
Paolo Savoldi <paooolino@gmail.com>
2016-07-27 16:01:41 +0200

    reducerTreeManagement tests

--- back-end/src/reducers/reducerTreeManagement.js
+++ back-end/src/reducers/reducerTreeManagement.js
@@ -1,1 +1,62 @@
+/*
+  internal imports
+*/
 
+import * as types from '../actionTypes';
+
+/*
+  setup
+*/
+
+const initialState = {
+  fieldNewItemNameInput: '',
+  items: [],
+  requesting: false,
+  errorMessage: '',
+  selectedId: 0,
+  invalidated: false
+};
+
+/*
+  reducer
+*/
+
+export default (state=initialState, action) => {
+  switch(action.type) {
+    
+    case types.CHANGE_FIELD_NEW_ITEM_NAME_INPUT:
+      return {
+        ...initialState,
+        fieldNewItemNameInput: action.value
+      };    
+      break;
+      
+    case types.ADD_ITEM_REQUEST:
+      return {
+        ...initialState,
+        requesting: true
+      };  
+      break;
+
+    case types.ADD_ITEM_FAILURE:
+      return {
+        ...initialState,
+        requesting: false,
+        errorMessage: action.errorMessage
+      };  
+      break;
+      
+    case types.ADD_ITEM_SUCCESS:
+      return {
+        ...initialState,
+        selectedId: action.lastInsertedId,
+        requesting: false,
+        errorMessage: '',
+        invalidated: true
+      };
+      break;
+      
+    default:
+      return state;
+  }
+};
